73a8acec0a7d4976d288cb2bae2b8e209dc36b9f
Abhiram <55952410+Afterlifegamer@users.noreply.github.com>
2026-05-01 20:40:02 +0530

    Deployment

--- agri-setu-master/data/cart.js
+++ agri-setu-master/data/cart.js
@@ -5,17 +5,7 @@ loadFromStorage();
 export function loadFromStorage() {
   cart = JSON.parse(localStorage.getItem('cart'));
   if (!cart) {
-    cart = [
-      {
-        productId: "2",
-        quantity: 2,
-
-      },
-      {
-        productId: "3",
-        quantity: 1,
-
-      }];
+    cart = [];
   }
 }
 
